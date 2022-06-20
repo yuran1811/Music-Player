5aa038313b6ad6458a19d8d93a188dfe7af624b0
yuran1811 <trieuvanbd123@gmail.com>
2022-06-20 14:21:09 +0700

    fix bugs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 's12kPWA-ver1';
+const cacheName = 'm9r-s12k-PWA-ver1';
 const appShellFiles = ['/index.html', '/src/js/script.js', '/src/js/render.js', '/src/css/index.css', '/src/img/Logo.png'];
 
 self.addEventListener('install', (e) => {
@@ -15,15 +15,12 @@ self.addEventListener('fetch', (e) => {
 	e.respondWith(
 		(async () => {
 			const r = await caches.match(e.request);
-
-			console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-
 			if (r) return r;
 
 			const response = await fetch(e.request);
 			const cache = await caches.open(cacheName);
 
-			console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+			console.log(`[Service Worker] : Fetching and caching new resource - ${e.request.url}`);
 
 			cache.put(e.request, response.clone()).catch(console.log);
 
